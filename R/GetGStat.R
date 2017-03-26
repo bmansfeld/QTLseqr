24f65efdb7e0d117424122c98e3263304b61e7fb
bmansfeld <bmansfeld@gmail.com>
2017-03-26 10:07:04 -0400

    Pass only the numeric cols to apply.
    Added GetGStat2 which assumes expected allele counts to be equal with out accounting for read depth differences between samples.

--- R/GetGStat.R
+++ R/GetGStat.R
@@ -1,11 +1,22 @@
-GetGStat<-function(VarSet){
-#Calculates G statistic for each SNP.     
-    
-    VarSet$GStat<-apply(VarSet,1,function(x){
-        obs<-c(as.numeric(x[18]),as.numeric(x[9]),as.numeric(x[19]),as.numeric(x[10]))
-        exp<-c(0.5*as.numeric(x[15]),0.5*as.numeric(x[6]),0.5*as.numeric(x[15]),0.5*as.numeric(x[6]))
-        2 * sum(obs * log(obs / exp),na.rm = T)
-    })
-    VarSet
-    
+GetGStat <- function(VarSet) {
+  VarSet$GStat <- apply(VarSet[,5:ncol(VarSet)], 1, function(x) {
+    obs <-
+      c((x["AD_REF.LOW"]), (x["AD_REF.HIGH"]), (x["AD_ALT.LOW"]), (x["AD_ALT.HIGH"]))
+    exp <-
+      c(0.5 * (x["DP.LOW"]), 0.5 * (x["DP.HIGH"]), 0.5 * (x["DP.LOW"]), 0.5 * (x["DP.HIGH"]))
+    2 * sum(obs * log(obs / exp), na.rm = T)
+  })
+  VarSet
+  
+}
+
+GetGStat2 <- function(VarSet) {
+  VarSet$GStat2 <- apply(VarSet[,5:ncol(VarSet)], 1, function(x) {
+    obs <-
+      c((x["AD_REF.LOW"]), (x["AD_REF.HIGH"]), (x["AD_ALT.LOW"]), (x["AD_ALT.HIGH"]))
+    exp <- rep((((x["AD_REF.LOW"]) + (x["AD_ALT.LOW"])) * ((x["AD_REF.HIGH"]) + (x["AD_ALT.HIGH"]))) / sum(obs), 4)
+    2 * sum(obs * log(obs / exp), na.rm = T)
+  })
+  VarSet
+  
 }
