450e4ffbbcbd271a949172d2d7e61fc77c8e66ad
bmansfeld <bmansfeld@gmail.com>
2017-03-26 15:10:59 -0400

    Changed High- LowSample to High- LowBulk

--- R/import_GATK_VCF.R
+++ R/import_GATK_VCF.R
@@ -1,38 +1,38 @@
 #import vcf from GATK VarToTable
-import_GATK_VCF <- function(file,
-                            HighSample = character(),
-                            LowSample = character(),
+import_GATK_VCF <- function(filename,
+                            HighBulk = character(),
+                            LowBulk = character(),
                             ChromList = NULL) {
   VarTable <-
-    read.table(file = file,
+    read.table(file = filename,
                header = T,
                stringsAsFactors = F)
-  
+
    # Format data frame for analysis
   SNPset <- VarTable[, 1:4]
-  
+
   # High Bulk data
-  SNPset$DP.HIGH <- VarTable[, paste0(HighSample, ".DP")]
+  SNPset$DP.HIGH <- VarTable[, paste0(HighBulk, ".DP")]
   SNPset$AD_REF.HIGH <-
-    as.numeric(gsub(",.*$", "", x = VarTable[, paste0(HighSample, ".AD")]))
+    as.numeric(gsub(",.*$", "", x = VarTable[, paste0(HighBulk, ".AD")]))
   SNPset$AD_ALT.HIGH <- SNPset$DP.HIGH - SNPset$AD_REF.HIGH
-  SNPset$GQ.HIGH <- VarTable[, paste0(HighSample, ".GQ")]
+  SNPset$GQ.HIGH <- VarTable[, paste0(HighBulk, ".GQ")]
   # Calculate SNP index
   SNPset$SNPindex.HIGH <- SNPset$AD_ALT.HIGH / SNPset$DP.HIGH
-  
+
   # Low Bulk data
-  SNPset$DP.LOW <- VarTable[, paste0(LowSample, ".DP")]
+  SNPset$DP.LOW <- VarTable[, paste0(LowBulk, ".DP")]
   SNPset$AD_REF.LOW <-
-    as.numeric(gsub(",.*$", "", x = VarTable[, paste0(LowSample, ".AD")]))
+    as.numeric(gsub(",.*$", "", x = VarTable[, paste0(LowBulk, ".AD")]))
   SNPset$AD_ALT.LOW <- SNPset$DP.LOW - SNPset$AD_REF.LOW
-  SNPset$GQ.LOW <- VarTable[, paste0(LowSample, ".GQ")]
+  SNPset$GQ.LOW <- VarTable[, paste0(LowBulk, ".GQ")]
   SNPset$SNPindex.LOW <- SNPset$AD_ALT.LOW / SNPset$DP.LOW
-  
+
   #Subset any unwanted chromosomes
   SNPset <- subset(SNPset, CHROM %in% ChromList)
-  
-  # Calculate some descripters 
+
+  # Calculate some descripters
   SNPset$REF_FRQ <- (SNPset$AD_REF.HIGH + SNPset$AD_REF.LOW) / (SNPset$DP.HIGH + SNPset$DP.LOW)
-  SNPset$deltaSNP <- SNPset$SNPindex.HIGH - SNPset$SNPindex.LOW 
+  SNPset$deltaSNP <- SNPset$SNPindex.HIGH - SNPset$SNPindex.LOW
   return(SNPset)
 }
