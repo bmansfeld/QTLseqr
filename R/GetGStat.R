e8921168d41fd839dca82051f3f1e68d9ad71abb
bmansfeld <bmansfeld@gmail.com>
2017-03-30 13:04:43 -0400

    Changed VarSet to SNPset

--- R/GetGStat.R
+++ R/GetGStat.R
@@ -1,22 +1,22 @@
-GetGStat <- function(VarSet) {
-  VarSet$GStat <- apply(VarSet[,5:ncol(VarSet)], 1, function(x) {
+GetGStat <- function(SNPset) {
+  SNPset$GStat <- apply(SNPset[,5:ncol(SNPset)], 1, function(x) {
     obs <-
       c((x["AD_REF.LOW"]), (x["AD_REF.HIGH"]), (x["AD_ALT.LOW"]), (x["AD_ALT.HIGH"]))
     exp <-
       c(0.5 * (x["DP.LOW"]), 0.5 * (x["DP.HIGH"]), 0.5 * (x["DP.LOW"]), 0.5 * (x["DP.HIGH"]))
     2 * sum(obs * log(obs / exp), na.rm = T)
   })
-  VarSet
-  
+  SNPset
+
 }
 
-GetGStat2 <- function(VarSet) {
-  VarSet$GStat2 <- apply(VarSet[,5:ncol(VarSet)], 1, function(x) {
+GetGStat2 <- function(SNPset) {
+  SNPset$GStat2 <- apply(SNPset[,5:ncol(SNPset)], 1, function(x) {
     obs <-
       c((x["AD_REF.LOW"]), (x["AD_REF.HIGH"]), (x["AD_ALT.LOW"]), (x["AD_ALT.HIGH"]))
     exp <- rep((((x["AD_REF.LOW"]) + (x["AD_ALT.LOW"])) * ((x["AD_REF.HIGH"]) + (x["AD_ALT.HIGH"]))) / sum(obs), 4)
     2 * sum(obs * log(obs / exp), na.rm = T)
   })
-  VarSet
-  
+  SNPset
+
 }
