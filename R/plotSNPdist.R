39c829d5a8c344655624060b67d8d524109e611f
bmansfeld <bmansfeld@gmail.com>
2017-04-24 09:47:58 -0400

    Added a step size  argument for the sliding window. More appropriate

--- R/plotSNPdist.R
+++ R/plotSNPdist.R
@@ -1,14 +1,14 @@
-plotSNPdist <- function(VarSet, WinSize=1e6){
+plotSNPdist <- function(VarSet, WinSize=1e6, StepSize=1e4){
       #Plots the number of SNPs per WinSize
-      
+
       SW<-data.frame()
       for (x in levels(as.factor(VarSet$CHROM))) {
-            
+
             chr<-as.data.frame(subset(VarSet, CHROM == x))
-            
+
             #create sliding window step bins
-            bin <- seq(1,max(chr[,"POS"]),WinSize) 
-            
+            bin <- seq(1,max(chr[,"POS"]),StepSize)
+
             count<-sapply(1:(length(bin)),
                           function(i)
                                 nrow(chr[chr$POS>=bin[i]-(0.5*WinSize)  &
