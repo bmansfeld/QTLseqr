a57dde36983b0177e275d9d3114f216a46ecec86
bmansfeld <bmansfeld@gmail.com>
2017-06-25 19:11:58 -0400

    ?

--- R/format_genomic.R
+++ R/format_genomic.R
@@ -12,17 +12,17 @@ format_genomic <- function(...) {
       #   A function to format a vector of strings using
       #   SI prefix notation
       #
-      
+
       function(x) {
             limits <- c(1e0,   1e3, 1e6)
             prefix <- c("","Kb","Mb")
-            
+
             # Vector with array indices according to position in intervals
             i <- findInterval(abs(x), limits)
-            
+
             # Set prefix to " " for very small values < 1e-24
             i <- ifelse(i==0, which(limits == 1e0), i)
-            
+
             paste(format(round(x/limits[i], 1),
                          trim=TRUE, scientific=FALSE, ...),
                   prefix[i])
