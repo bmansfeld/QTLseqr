a168a31bff4e9a3da71d733a9a17016d86268234
bmansfeld <bmansfeld@gmail.com>
2017-10-12 15:20:38 -0400

    filter threshold

--- vignettes/QTLseqr.R
+++ vignettes/QTLseqr.R
@@ -3,154 +3,15 @@ knitr::opts_chunk$set(tidy=FALSE, cache=TRUE,
                       dev="png",
                       message=FALSE, error=FALSE, warning=TRUE)
 
-## ----quickStart, eval=FALSE----------------------------------------------
-#  
-#  #load the package
-#  library("QTLseqr")
-#  
-#  #Set sample and file names
-#  HighBulk <- "SRR834931"
-#  LowBulk <- "SRR834927"
-#  file <- "SNPs_from_GATK.table"
-#  
-#  #Choose which chromosomes will be included in the analysis (i.e. exclude smaller contigs)
-#  Chroms <- paste0(rep("Chr", 12), 1:12)
-#  
-#  #Import SNP data from file
-#  df <-
-#      importFromGATK(
-#          filename = file,
-#          highBulk = HighBulk,
-#          lowBulk = LowBulk,
-#          chromList = Chroms
-#       )
-#  
-#  #Filter SNPs based on some criteria
-#  df_filt <-
-#      filterSNPs(
-#          SNPset = df,
-#          refAlleleFreq = 0.20,
-#          minTotalDepth = 100,
-#          maxTotalDepth = 400,
-#          minSampleDepth = 40,
-#          minGQ = 99
-#      )
-#  
-#  
-#  #Run G' analysis
-#  df_filt <- runGprimeAnalysis(
-#      SNPset = df_filt,
-#      windowSize = 1e6,
-#      outlierFilter = "deltaSNP")
-#  
-#  #Plot
-#  plotQTLStats(SNPset = df_filt, var = "deltaSNP", plotThreshold = TRUE, q = 0.01)
-#  plotQTLStats(SNPset = df_filt, var = "Gprime", plotThreshold = TRUE, q = 0.01)
-#  
-#  #export summary CSV
-#  getQTLTable(SNPset = df_filt, alpha = 0.01, export = TRUE, fileName = "my_BSA_QTL.csv")
-
-## ------------------------------------------------------------------------
-library("QTLseqr")
-rawData <- system.file("extdata", "Yang_et_al_2013.table", package = "QTLseqr", mustWork = TRUE)
-
-## ---- eval = FALSE-------------------------------------------------------
-#  rawData <- "C:/PATH/TO/MY/DIR/My_BSA_data.table"
-
-## ------------------------------------------------------------------------
-HighBulk <- "SRR834931"
-LowBulk <- "SRR834927"
-Chroms <- paste0(rep("Chr", 12), 1:12)
-
-## ----import, cache=TRUE--------------------------------------------------
-#import data
-df <-
-    importFromGATK(
-        filename = rawData,
-        highBulk = HighBulk,
-        lowBulk = LowBulk,
-        chromList = Chroms
-     )
-
-## ----viewdf--------------------------------------------------------------
-head(df)
-
-## ----plothist1, warning = FALSE, fig.align="center", fig.width=4, fig.height=4----
-library("ggplot2")
-ggplot(data = df) + 
-    geom_histogram(aes(x = DP.HIGH + DP.LOW)) + 
-    xlim(0,1000)
-
-
-## ----plothist2, warning=FALSE, fig.align="center", fig.width=4, fig.height=4----
-ggplot(data = df) +
-    geom_histogram(aes(x = REF_FRQ))
-
-## ----filtSNPs-source, eval = FALSE, message = FALSE----------------------
-#  df_filt <-
-#      filterSNPs(
-#          SNPset = df,
-#          refAlleleFreq = 0.20,
-#          minTotalDepth = 100,
-#          maxTotalDepth = 400,
-#          minSampleDepth = 40,
-#          minGQ = 99,
-#          verbose = TRUE
-#      )
-
-## ----filtSNPs-msgs, message = TRUE, warning = FALSE, collapse = TRUE, echo = FALSE----
-df_filt <-
-    filterSNPs(
-        SNPset = df,
-        refAlleleFreq = 0.20,
-        minTotalDepth = 100,
-        maxTotalDepth = 400,
-        minSampleDepth = 40,
-        minGQ = 99,
-        verbose = TRUE
-    )
-
 ## ----gprimeanalysis-src, eval = FALSE------------------------------------
 #  df_filt <- runGprimeAnalysis(df_filt,
 #      windowSize = 1e6,
-#      outlierFilter = "deltaSNP")
+#      outlierFilter = "deltaSNP",
+#      filterThreshold = 0.1)
 
 ## ----gprimeanalysis-msg, message = TRUE, warning = FALSE, collapse = TRUE, echo = FALSE----
 df_filt <- runGprimeAnalysis(df_filt,
     windowSize = 1e6,
-    outlierFilter = "deltaSNP")
-
-## ------------------------------------------------------------------------
-head(df_filt)
-
-## ----gprimedist hampel, message = FALSE, warning = FALSE, fig.align = "center", fig.height=4----
-plotGprimeDist(SNPset = df_filt, outlierFilter = "Hampel")
-
-
-## ----gprimedist deltaSNP, message=FALSE, warning = FALSE, fig.align = "center", fig.height=4----
-plotGprimeDist(SNPset =df_filt, outlierFilter = "deltaSNP")
-
-## ----plotnSNPs, fig.align="center", fig.width=12, fig.height=4-----------
-p1 <- plotQTLStats(SNPset = df_filt, var = "nSNPs")
-p1
-
-## ----plotdeltaSNP, fig.align="center", fig.width=12, fig.height=4--------
-p2 <- plotQTLStats(SNPset = df_filt, var = "deltaSNP")
-p2
-
-## ----plotGprime, fig.align="center", fig.width=12, fig.height=4----------
-p3 <- plotQTLStats(SNPset = df_filt, var = "Gprime", plotThreshold = TRUE, q = 0.01)
-p3
-
-## ----subsetlogpval, fig.align="center", fig.width=6, fig.height=4--------
-QTLplots <- plotQTLStats(SNPset = df_filt, var = "negLog10Pval", plotThreshold = TRUE, q = 0.01, subset = c("Chr1", "Chr8"))
-QTLplots
-
-## ----getsigreg-----------------------------------------------------------
-QTL <- getSigRegions(SNPset = df_filt, alpha = 0.01)
-head(QTL[[1]])
-
-## ----QTLtable------------------------------------------------------------
-results <- getQTLTable(SNPset = df_filt, alpha = 0.01, export = FALSE)
-results
+    outlierFilter = "deltaSNP",
+    filterThreshold = 0.1)
 
