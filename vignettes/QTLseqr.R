3ea7164f5895b0b3d7bd34d0d43ca6c68cc92564
bmansfeld <bmansfeld@gmail.com>
2017-10-13 14:31:31 -0400

    filterThresh stuff

--- vignettes/QTLseqr.R
+++ vignettes/QTLseqr.R
@@ -3,15 +3,3 @@ knitr::opts_chunk$set(tidy=FALSE, cache=TRUE,
                       dev="png",
                       message=FALSE, error=FALSE, warning=TRUE)
 
-## ----gprimeanalysis-src, eval = FALSE------------------------------------
-#  df_filt <- runGprimeAnalysis(df_filt,
-#      windowSize = 1e6,
-#      outlierFilter = "deltaSNP",
-#      filterThreshold = 0.1)
-
-## ----gprimeanalysis-msg, message = TRUE, warning = FALSE, collapse = TRUE, echo = FALSE----
-df_filt <- runGprimeAnalysis(df_filt,
-    windowSize = 1e6,
-    outlierFilter = "deltaSNP",
-    filterThreshold = 0.1)
-
